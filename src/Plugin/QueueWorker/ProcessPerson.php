ad90e3f8751420a48b5df1496392361a043af32d
Campbell Vertesi <campbell@vertesi.com>
2018-07-20 17:38:00 +0200

    Add other queue workers

--- src/Plugin/QueueWorker/ProcessPerson.php
+++ src/Plugin/QueueWorker/ProcessPerson.php
@@ -5,17 +5,17 @@ declare(strict_types = 1);
 namespace Drupal\image_auto_tag\Plugin\QueueWorker;
 
 /**
- * Queue Worker for image_auto_tag_detect_faces.
+ * Queue Worker for image_auto_tag_process_person.
  *
  * @package Drupal\image_auto_tag\Plugin\QueueWorker
  *
  * @QueueWorker(
- *   id = "image_auto_tag_detect_faces",
- *   title = @Translation("Detect Faces"),
+ *   id = "image_auto_tag_process_person",
+ *   title = @Translation("Process Person"),
  *   cron = {"time" = 10}
  * )
  */
-class DetectFaces extends QueueWorkerBase {
+class ProcessPerson extends QueueWorkerBase {
 
   /**
    * {@inheritdoc}
@@ -23,8 +23,7 @@ class DetectFaces extends QueueWorkerBase {
   public function processItem($data) {
     /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
     $entity = $this->entityTypeManager->getStorage($data['entityType'])->load($data['entityId']);
-    $fieldDefinition = $entity->getFieldDefinition($data['fieldName']);
-    $this->entityOperations->findFacesAndTag($entity, $fieldDefinition);
+    $this->entityOperations->processPerson($entity);
     $entity->save();
   }
 
